ec15fe67cc76e149213f723235b4c819ef245b5c
Tomasz Stachowski <thegreyraveen@gmail.com>
2020-03-26 15:28:27 +0100

    auto

--- trace.h
+++ trace.h
@@ -25,7 +25,9 @@ void construct_sockaddr(struct sockaddr_in *address, sa_family_t family, char *a
 void construct_icmphdr(struct icmphdr *header, uint8_t type, uint8_t code, uint16_t id, uint16_t sequence);
 void reset_replies(int n, struct reply array[n]);
 void send_probes(int sockfd, struct sockaddr_in dest, int ttl, int probes, uint16_t id, uint16_t *seq_ptr);
-void reset_time(struct timeval *tv);
+void set_time(struct timeval *tv, time_t sec, suseconds_t usec);
 int check_for_answers(int sockfd, int ttl, uint16_t id, uint16_t probes_per_turn, struct reply replies[probes_per_turn]);
 void analize_packet(u_int8_t *buffer, uint8_t *returned_type_p, uint16_t *returned_id_p, uint16_t *returned_seq_p);
 void receive_packets(int sockfd, int ttl, uint16_t id, uint16_t probes_per_turn, struct reply replies[probes_per_turn], int *packets_left_ptr, struct timeval tv, int *destination_reached);
+void print_traceroute(uint16_t probes_per_turn, struct reply replies[probes_per_turn], uint16_t ttl);
+
